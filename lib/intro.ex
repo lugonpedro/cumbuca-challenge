3fe0db65e0289bbcfaa378597c2640f130dfb119
Pedro Lugon <pedromotta20@gmail.com>
2024-07-16 16:54:03 -0300

    fix(intro): improving description

--- lib/intro.ex
+++ lib/intro.ex
@@ -1,5 +1,11 @@
 defmodule Intro do
   def main() do
-    IO.puts("Digite strings (deixe em branco e pressione Enter para terminar):")
+    IO.puts("Digite o nome do rei ou rainha e pressione Enter.")
+
+    IO.puts("Ao terminar a lista apenas pressione Enter sem digitar nenhum nome.")
+
+    IO.puts("* Diferenças entre letras maiúsculas e minúsculas serão desconsideradas.")
+
+    IO.puts("* Já acentuação será considerado como diferença entre os nomes.")
   end
 end
