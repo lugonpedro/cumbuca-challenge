a91230c7296d1bf6ef27db32b4071cdba6449c22
Pedro Lugon <pedromotta20@gmail.com>
2024-07-16 17:09:56 -0300

    fix(list): change order for better experience

--- lib/show_list.ex
+++ lib/show_list.ex
@@ -1,12 +1,14 @@
 defmodule ShowList do
   def main(list) do
+    IO.puts("Essa é a lista real:")
+
     Enum.with_index(list, fn name, index ->
       {list_before_index, _list_after_index} = Enum.split(list, index)
       quantity = Enum.count(list_before_index, fn el -> el == name end)
       IO.puts("#{name} #{RomanNumber.convert(quantity + 1)}")
+      "#{name} #{RomanNumber.convert(quantity + 1)}"
     end)
 
-    IO.puts("Essa é a sua lista real!")
     DesafioCli.restart()
   end
 end
